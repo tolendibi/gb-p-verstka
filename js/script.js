2af3f5eafd30164f94f5ffe375982d80576a31c2
tolendibi <50864756+tolendibi@users.noreply.github.com>
2022-04-22 19:08:47 +0600

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -12,13 +12,21 @@ meuclose.addEventListener('click', toggleMenu);
 /*app-js sart*/
 
 let filterPopup = document.querySelector('.filterPopup');
-
+let filtersIcon = document.querySelector('.filtersIcon');
 let filtersLabel = document.querySelector('.filtersLabel');
+
 filtersLabel.addEventListener('click', function() {
     filterPopup.classList.toggle('hiddenn');
+    filtersLabel.classList.toggle('filterslabelpink');
+    filtersIcon.classList.toggle('filterslabelpink');
+    if (filtersIcon.getAttribute('src') === '../images/filter.svg') {
+        filtersIcon.setAttribute('src','../images/filterhover.svg')
+    } else 
+    filtersIcon.setAttribute('src','../images/filter.svg')
 });
-let filtersIcon = document.querySelector('.filtersIcon');
-filtersIcon.addEventListener('click', function() {
-    filterPopup.classList.toggle('hiddenn');
-});
+// let filtersIcon = document.querySelector('.filtersIcon');
+// filtersIcon.addEventListener('click', function() {
+//     filterPopup.classList.toggle('hiddenn');
+//     filtersIcon.classList.toggle('filtersIconpink');
+// });
 /*app-js end*/
